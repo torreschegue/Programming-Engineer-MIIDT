df61e5f7280c023f64ef37937c01b311288080e8
Cristian Omar Torres <nem.cristian@coppel.com>
2025-10-09 01:38:41 -0600

    integracion codigo constante.cpp

---
+++ Semana_2/Ejemplos/constante.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const double PI = 3.1416;
+    double radio = 5;
+    double area = PI * radio * radio;
+
+    cout << "Área del círculo: " << area << endl;
+    return 0;
+}
